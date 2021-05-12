93c854eb30840fbfbc959ea096ebfc99e4d5eebc
Miguel Rios <mrios0495@gmail.com>
2021-05-12 17:58:14 -0400

    Defines the SongFileManager and its upload method.

---
+++ src/Manager/SongFileManager.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Manager;
+
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class SongFileManager
+{
+    protected string $targetDirectory;
+    protected SluggerInterface $slugger;
+
+    public function __construct(string $targetDirectory, SluggerInterface $slugger)
+    {
+        $this->targetDirectory = $targetDirectory;
+        $this->slugger = $slugger;
+    }
+
+    public function upload(UploadedFile $file): string
+    {
+        $uploadedFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $sanitizedFilename = $this->slugger->slug($uploadedFilename);
+        $songFileName = $sanitizedFilename . '-' . uniqid() . '.' . $file->guessExtension();
+
+        try {
+            $file->move($this->targetDirectory, $songFileName);
+        } catch (FileException $e) {
+            # Just rethrowing for now
+            throw $e;
+        }
+
+        return $songFileName;
+    }
+
+    public function delete(string $fileName)
+    {
+
+    }
+}
+
+
